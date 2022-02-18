2a1e3cf7158bc0e31f43d98f0d056314db9546e6
Guilherme Oliani <guilhermenoliani@gmail.com>
2022-02-18 17:27:51 -0300

    Completo

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const openToggle = () => {
+  let menu = document.querySelector('#menu-area');
+  if(menu.style.width == '200px'){
+    menu.style.width = '0px';
+  }else {
+    menu.style.width = '200px';
+  }
+}
