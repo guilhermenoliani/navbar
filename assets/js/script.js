a5f87bf260e9b38484e17155f0292f6d29832efb
Guilherme Oliani <guilhermenoliani@gmail.com>
2022-02-21 15:33:26 -0300

    Completo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
+//Título página
+let h1 = document.querySelector("h1");
+h1.innerHTML = "Simple Navbar";
+
+//Função navbar
 const openToggle = () => {
-  let menu = document.querySelector('#menu-area');
-  if(menu.style.width == '200px'){
-    menu.style.width = '0px';
-  }else {
-    menu.style.width = '200px';
+  let menu = document.querySelector("#menu-area");
+  if (menu.style.width == "200px") {
+    menu.style.width = "0px";
+  } else {
+    menu.style.width = "200px";
   }
-}
+};
